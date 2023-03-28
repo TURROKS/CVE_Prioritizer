6ffa9810e1389f9d605c5e0f702d7cf69b4a910d
Mario Rojas <16016384+TURROKS@users.noreply.github.com>
2023-03-28 21:17:21 +0400

    Added argparse options

--- cve_prioritizer.py
+++ cve_prioritizer.py
@@ -6,15 +6,29 @@ __version__ = "1.0.0"
 __maintainer__ = "Mario Rojas"
 __status__ = "Development"
 
+import argparse
 import threading
 
 from scripts.helpers import nist_check
 from scripts.helpers import epss_check
 
+# argparse setup
+parser = argparse.ArgumentParser(description="CVE Prioritizer tool", epilog='Happy Patching')
+parser.add_argument('-c', '--cve', type=str, help='Unique CVE ID', required=False, metavar='')
+parser.add_argument('-e', '--epss', type=float, help='EPSS Threshold (Default 0.2)', default=0.2, metavar='')
+parser.add_argument('-f', '--file', type=str, help='TXT File with CVE IDs (One per Line)', required=False, metavar='')
+parser.add_argument('-l', '--list', type=str, help='Comma Separated List of CVE IDs', required=False, metavar='')
+parser.add_argument('-n', '--cvss', type=float, help='CVSS Threshold (Default 7.0)', default=7.0, metavar='')
+parser.add_argument('-o', '--output', type=str, help='Output Filename', required=False, metavar='')
+parser.add_argument('-t', '--threads', type=str, help='Number of Threads', required=False, metavar='')
+parser.add_argument('-v', '--verbose', type=str, help='Verbose Mode', default=False, metavar='')
+
+# Global Arguments
+args = parser.parse_args()
+
 
 def main(cve_id):
 
-    # cisa_result = cisa_check(cve_id)
     nist_result = nist_check(cve_id)
     epss_result = epss_check(cve_id)
 
