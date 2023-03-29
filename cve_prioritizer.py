2d6f968581584a1a97f16afd0c5dffe162fc37b7
Mario Rojas <16016384+TURROKS@users.noreply.github.com>
2023-03-29 10:51:42 +0400

    Implemented --cve and --list parsing functions

--- cve_prioritizer.py
+++ cve_prioritizer.py
@@ -11,13 +11,16 @@ import threading
 
 from scripts.helpers import nist_check
 from scripts.helpers import epss_check
+from scripts.helpers import list_parser
 
 # argparse setup
-parser = argparse.ArgumentParser(description="CVE Prioritizer tool", epilog='Happy Patching')
+parser = argparse.ArgumentParser(description="CVE Prioritizer tool", epilog='Happy Patching',
+                                 usage='cve_prioritizer.py -c CVE-XXXX-XXXX')
 parser.add_argument('-c', '--cve', type=str, help='Unique CVE ID', required=False, metavar='')
 parser.add_argument('-e', '--epss', type=float, help='EPSS Threshold (Default 0.2)', default=0.2, metavar='')
-parser.add_argument('-f', '--file', type=str, help='TXT File with CVE IDs (One per Line)', required=False, metavar='')
-parser.add_argument('-l', '--list', type=str, help='Comma Separated List of CVE IDs', required=False, metavar='')
+parser.add_argument('-f', '--file', type=argparse.FileType('r'), help='TXT File with CVE IDs (One per Line)',
+                    required=False, metavar='')
+parser.add_argument('-l', '--list', help='Space Separated List of CVE IDs', nargs='+', required=False, metavar='')
 parser.add_argument('-n', '--cvss', type=float, help='CVSS Threshold (Default 7.0)', default=7.0, metavar='')
 parser.add_argument('-o', '--output', type=str, help='Output Filename', required=False, metavar='')
 parser.add_argument('-t', '--threads', type=str, help='Number of Threads', required=False, metavar='')
@@ -27,35 +30,49 @@ parser.add_argument('-v', '--verbose', type=str, help='Verbose Mode', default=Fa
 args = parser.parse_args()
 
 
-def main(cve_id):
+def main(cve_id, cvss_score, epss_score):
 
     nist_result = nist_check(cve_id)
     epss_result = epss_check(cve_id)
 
-    if nist_result[1] is True:
-        print(f"{cve_id:<18}Priority 1+")
-    elif nist_result[0] >= 7.0:
-        if epss_result >= 0.2:
-            print(f"{cve_id:<18}Priority 1")
+    try:
+        if nist_result[1] is True:
+            print(f"{cve_id:<18}Priority 1+")
+        elif nist_result[0] >= cvss_score:
+            if epss_result >= epss_score:
+                print(f"{cve_id:<18}Priority 1")
+            else:
+                print(f"{cve_id:<18}Priority 2")
         else:
-            print(f"{cve_id:<18}Priority 2")
-    else:
-        if epss_result >= 0.2:
-            print(f"{cve_id:<18}Priority 3")
-        else:
-            print(f"{cve_id:<18}Priority 4")
+            if epss_result >= epss_score:
+                print(f"{cve_id:<18}Priority 3")
+            else:
+                print(f"{cve_id:<18}Priority 4")
+    except TypeError:
+        print("Unable to fetch results, check your internet connection or Input")
 
 
 if __name__ == '__main__':
 
     num_threads = 3
-    cves = ["CVE-2017-16885", "CVE-2020-29127", "CVE-2020-4657", "CVE-2019-0808", "CVE-2023-23397"]
+    # cves = ["CVE-2017-16885", "CVE-2020-29127", "CVE-2020-4657", "CVE-2019-0808", "CVE-2023-23397"]
+    cve_list = []
 
     threads = []
+    epss_threshold = args.epss
+    cvss_threshold = args.cvss
 
-    for cve in cves:
+    if args.cve:
+        cve_list.append(args.cve)
+    elif args.list:
+        cve_list = args.list
+        print(cve_list)
+    elif args.file:
+        cve_list = [line.rstrip() for line in args.file]
 
-        t = threading.Thread(target=main, args=(cve,))
+    for cve in cve_list:
+
+        t = threading.Thread(target=main, args=(cve, cvss_threshold, epss_threshold,))
         threads.append(t)
         t.start()
 
