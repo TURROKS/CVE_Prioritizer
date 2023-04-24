e3ece483ee302d489ff00fa2bf91381d4b36d880
Mario Rojas <16016384+TURROKS@users.noreply.github.com>
2023-04-24 18:34:46 +0400

    Included information on CVEs awaiting analysis

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -75,6 +75,8 @@ def nist_check(cve_id):
                                        "cvss_severity": metric.get("cvssData").get("baseSeverity"),
                                        "cisa_kev": cisa_kev}
                             return results
+                    elif unique_cve.get("cve").get("vulnStatus") != "Analyzed":
+                        print(f"{cve_id:<18}{unique_cve.get('cve').get('vulnStatus')}")
             else:
                 print(f"{cve_id:<18}Not Found in NIST NVD.")
         else:
