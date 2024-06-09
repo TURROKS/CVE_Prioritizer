7c69e70265e70467411da112cdb765ac51611e9b
Mario Rojas <16016384+TURROKS@users.noreply.github.com>
2024-06-09 16:38:07 +0400

    Cleaned up

--- scripts/constants.py
+++ scripts/constants.py
@@ -6,13 +6,17 @@ __version__ = "1.5.4"
 __maintainer__ = "Mario Rojas"
 __status__ = "Production"
 
+# API URLs
+EPSS_URL = "https://api.first.org/data/v1/epss"
+NIST_BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+NUCLEI_BASE_URL = "https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/main/cves.json"
+VULNCHECK_BASE_URL = "https://api.vulncheck.com/v3/index/nist-nvd2"
+VULNCHECK_KEV_BASE_URL = "https://api.vulncheck.com/v3/index/vulncheck-kev"
+
+# Visuals
 SIMPLE_HEADER = f"{'CVE-ID':<18}Priority"+"\n"+("-"*30)
 VERBOSE_HEADER = (f"{'CVE-ID':<18}{'PRIORITY':<13}{'EPSS':<9}{'CVSS':<6}{'VERSION':<10}{'SEVERITY':<10}{'KEV':<10}"
                   f"{'VENDOR':<18}{'PRODUCT':<23}VECTOR")+"\n"+("-"*162)
-EPSS_URL = "https://api.first.org/data/v1/epss"
-NIST_BASE_URL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
-VULNCHECK_BASE_URL = "https://api.vulncheck.com/v3/index/nist-nvd2"
-VULNCHECK_KEV_BASE_URL = "https://api.vulncheck.com/v3/index/vulncheck-kev"
 LOGO = r"""
 #    ______   ______                         
 #   / ___/ | / / __/                         
